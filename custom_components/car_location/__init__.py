f8b35f055149acc409a486fe0cccf51d09774ebe
vlkvsky <vlkvsky@gmail.com>
2026-04-27 20:25:46 +0300

    Create __init__.py

---
+++ custom_components/car_location/__init__.py
@@ -0,0 +1,29 @@
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from .coordinator import CarDataCoordinator
+
+DOMAIN = "car_location"
+
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    coordinator = CarDataCoordinator(hass, entry.data)
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry, ["device_tracker"]
+    )
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    return await hass.config_entries.async_unload_platforms(
+        entry, ["device_tracker"]
+    )
