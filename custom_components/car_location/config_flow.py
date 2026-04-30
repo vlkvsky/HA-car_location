1227f4ac9bff99f6b0414b8ce84ea889a0bbe4e9
vlkvsky <vlkvsky@gmail.com>
2026-04-30 12:09:35 +0300

    v3_beta

--- custom_components/car_location/config_flow.py
+++ custom_components/car_location/config_flow.py
@@ -23,27 +23,3 @@ class CarLocationFlow(config_entries.ConfigFlow, domain=DOMAIN):
         })
 
         return self.async_show_form(step_id="user", data_schema=schema)
-
-    @staticmethod
-    def async_get_options_flow(config_entry):
-        return CarLocationOptionsFlow(config_entry)
-
-
-class CarLocationOptionsFlow(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        if user_input is not None:
-            return self.async_create_entry(title="", data=user_input)
-
-        current_interval = self.config_entry.options.get(
-            "scan_interval",
-            self.config_entry.data.get("scan_interval", 60)
-        )
-
-        schema = vol.Schema({
-            vol.Optional("scan_interval", default=current_interval): int,
-        })
-
-        return self.async_show_form(step_id="init", data_schema=schema)
